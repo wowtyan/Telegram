68f1bc42786078c51885a80c8d677e25f44e21c2
Evgeniy <34138242+wowtyan@users.noreply.github.com>
2018-08-15 03:00:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,58 +1,82 @@
-﻿from aiogram.types import inline_keyboard
+from aiogram.types import inline_keyboard
 
-token = '--ТОКЕН БОТА--'
-welcome_message = '''Привет! Я выгодно выделяю Вас среди менеджеров Telegram.
-За счет подачи сообщений сразу с кнопками ответов для админов..
-Воспользуйтесь командами для управления:
+token = '658763297:AAGjIp1W_GerLXrx6WwxL0hVy4HVhuqswsk'
+welcome_message = '''Привет! Я выгодно выделяю тебя среди менеджеров
+за счет подачи сообщений с кнопками ответов для админов
 					 
 /help - инструкция, как мной управлять;
-/about - почитать историю моего рождения;
 /donate - поблагодарить разработчиков и поддержать проект.
 
 '''
-stickerpack_name = '-ИМЯ СТИКЕРПАКА-'
-yes_sticker = 2
-no_sticker = 12
+stickerpack_name = 'PocketHelperBotStickers'
+yes_sticker = 0
+no_sticker = 1
 
 
 keys = [
 	'Y[N>B{7pu.$GG5mQJ_cpr4=}c5xcc;mq',
-	'+)4(*_ZRs^xF.e+xtZPb;2DHoAQWOAof',
-	'---ЛИЦЕНЗИОННЫЕ КЛЮЧИ---',
-	'LFy8yx30,)sGr^93z)O}w${qs*0fU<a#']
+	'+-4-*_ZRs^xF.e+xtZPb;2DHoAQWOAof',
+	'-%K#<TiH5-E~_|sb"72BcsE8l~Jqp$7w',
+	'LFy8yx30,)sGr^93z)O}w${qs*0fU<a#',
+	'iNDz?g@?XX1ttzzHE@6PKkFM42HovNgj',
+	'WeVJj~ik?WG3gm$Ux7SAOcmhM0~4$vv$',
+	'wy}i}IrLB%hQ@Om5sLwq8Rid7DgOvs$X',
+	'CrV%jGr@}#kf2k0#Jt90koOuQqM?wmHM',
+	'|8mTi9d}AvV3C$0kWlzKdK*s0AeJFbmQ',
+	'pi~F9rFJXsXK?3zlWY6LwWMN4ZtkwxI{',
+	'nH@GXjkpaRV}Z@2b*oMS?Uo~lVp}$lyY',
+	'{|%}*mqAg04sldmolDz%y32~Scpue5k}',
+	'RS#c9?lsd5Oe#B{Cm8gYtJAlgLb~@NsW',
+	'?{#FuJgOGaG{EnJT6Ps4X4Ay9OF1lNrw',
+	'pOvo16@8X9J7F5|s{MpY~#0k8nK~wMPV',
+	'Hbar1@UXKn*M4QrCZ7XxTv1nj~q8$i40',
+	'~SawCi3fnCr?|NIb?xW7F9kWXQ~{vMFX',
+	'$03t~V%$8Got8*oem}5~zKODmrKNFFoa',
+	'jy#k3W74Y#N$MWiWlp0OLF6x2Oom*wJg',
+	'6ZnyC7pGNLI4$*lSr~z5ZJRlKKn%QS%x',
+	'970H#jF2asfEe3kH1XVmB3VJd8LBxlZT',
+	'~SWJlOuH#P*o{UcHu?q{F#NuMs3o#xWf',
+	'tsxK~ko}o|~6Vjx2z5eo*HdN2$#~SuB#',
+	'nfMPL@zjA~D|9rc|7HgEc|BK%jT*I0~i']
 
-donate_message = '''Вы можеете поблагодарить команду
-разработчиков, отправив донейшн.
-Напишите в комментариях к переводу @имя своего аккаунта. И
-вам придёт придёт лицензионный ключ. Отправьте его мне и я
-удалю автопостинг этого сообщения. Оставлю тебе только
-команду /donate если вдруг захочешь снова поддержать
-проект. Принимаем Visa/MasterCard, ЯндексДеньги, Qiwi,
-WebMoney, PayPal, VKpay, с баланса мобильного телефона.'''
+donate_key_message = '''Если тебе понравился бот, то
+лучшая благодарность для
+нас - поддержка рублём :)
+						
+От твоей обратной связи зависит скорость
+разработки новых ботов для менеджеров и
+админов. Принимаем: Visa/MasterCard,
+ЯндексДеньги, Qiwi, WebMoney, PayPal,
+VKpay, с баланса мобильного телефона.'''
 
+donate_message = '''Благодарю тебя, дорогой друг, что пользуешься ботом!
+Если тебе понравился бот, то лучшая благодарность для нас - поддержка рублём :)
+
+Чтобы отключить это авто-сообщение, жми "Выключить".'''
+
+autodonate_keyboard = inline_keyboard.InlineKeyboardMarkup(2)
+autodonate_keyboard.insert(inline_keyboard.InlineKeyboardButton(
+	text='Выключить', url='https://telegra.ph/Kak-otklyuchit-avto-soobshchenie-08-14'))
+autodonate_keyboard.insert(inline_keyboard.InlineKeyboardButton(
+	text='Поддержать', url='http://www.donationalerts.ru/r/tyanevgeniy'))
+	
 donate_keyboard = inline_keyboard.InlineKeyboardMarkup(1)
 donate_keyboard.insert(inline_keyboard.InlineKeyboardButton(
-	text='150 руб', url='http://www.donationalerts.ru/r/tyanevgeniy'))
-donate_keyboard.insert(inline_keyboard.InlineKeyboardButton(
-	text='Другая сумма', url='http://www.donationalerts.ru/r/tyanevgeniy'))
-
+	text='Поддержать', url='http://www.donationalerts.ru/r/tyanevgeniy'))
 
 help_message = '''Команды:
 /help - инструкция, как мной управлять;
-/about - почитать историю моего рождения;
-/donate - поблагодарить разработчиков и поддержать проект.
+
+/donate - поблагодарить и поддержать команду проекта.
 
 Инструкция по использованию бота тут:'''
 help_keyboard = inline_keyboard.InlineKeyboardMarkup(1)
 help_keyboard.insert(inline_keyboard.InlineKeyboardButton(
 	text='Прочитать', url='http://telegra.ph/Instrukciya-po-ispolzovaniyu-bota-08-03'))
 	
-about_message = '''Бот был готов через 2 дня после
-того, как 5 менеджеров сказали,
-что у них появилась проблема.
-Всю историю созздания и идею
-бота почитать тут:'''
+about_message = '''Все вопросы, пожелания и замечания по
+улучшению бота можно написать напрямую разработчику:'''
 about_keyboard = inline_keyboard.InlineKeyboardMarkup(1)
 about_keyboard.insert(inline_keyboard.InlineKeyboardButton(
-	text='Прочитать', url='http://telegra.ph/Kak-ya-poyavilsya-na-svet-bez-kesareva-08-03'))
+	text='Написать', url='t.me/superdev'))
 	
